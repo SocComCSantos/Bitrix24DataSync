dbf648ecf23fd3adf210764f6849364f16cadb00
Kleiton <kleitondev@gmail.com>
2024-04-24 17:33:25 +0100

    Laravel 11 e Filament

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Bitrix24DataSync
-Integração dinâmica de dados com Bitrix24 usando Laravel. Suporta grandes volumes e garante eficiência.
+
+## Step 1 - Configure
+First, make a copy of the example environment configuration file by running the following command:
+```bash
+cp .env.exemple .env
+```
+
+## Step 2 - Environment Setup
+Use the following command to set up your environment:
+```bash
+./vendor/bin/sail up
+```
+This command will start the necessary Docker containers and set up your environment for the project.
+
+## Step 3 - Access the Bash Shell in Docker
+To access the Bash shell within the Docker container, run the following command:
+```bash
+docker-compose exec app bash
+```
+This command allows you to interact with the container for any specific tasks you might need to perform.
+
+## Step 4 - Configuration and Dependencies
+Ensure that your application is properly configured and has all the required dependencies. Execute the following commands one by one:
+```bash
+chmod -R 777 storage && chmod -R 777 bootstrap && php artisan migrate --seed && php artisan storage:link && php artisan queue:work
---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $modelLabel = 'Utilizador';
+
+    protected static ?string $pluralModelLabel = 'Utilizadores';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->label('Nome')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('email')
+                            ->label('E-mail')
+                            ->email()
+                            ->required()
+                            ->unique(ignoreRecord: true)
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('password')
+                            ->label('Palavra-passe')
+                            ->required()
+                            ->hiddenOn('edit')
+                            ->password()
+                            ->maxLength(255),
+                    ])
+                    ->columns(2)
+                    ->columnSpan(['lg' => fn (?User $record) => $record === null ? 3 : 2]),
+
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\Placeholder::make('created_at')
+                            ->label('Criado')
+                            ->content(fn (User $record): ?string => $record->created_at?->diffForHumans()),
+
+                        Forms\Components\Placeholder::make('updated_at')
+                            ->label('Atualizado')
+                            ->content(fn (User $record): ?string => $record->updated_at?->diffForHumans()),
+                    ])
+                    ->columnSpan(['lg' => 1])
+                    ->hidden(fn (?User $record) => $record === null),
+            ])
+            ->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nome')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('E-mail')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->label('E-mail verificado')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Criado')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Atualizado')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+}
