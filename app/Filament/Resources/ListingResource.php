5e1387f2fd5080a629e6b040d563648a2480138b
Kleiton <kleitondev@gmail.com>
2024-04-30 18:00:35 +0100

    Repeater

---
+++ app/Filament/Resources/ListingResource.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ListingResource\Pages;
+use App\Filament\Resources\ListingResource\RelationManagers;
+use App\Models\Listing;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ListingResource extends Resource
+{
+    protected static ?string $model = Listing::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-bookmark-square';
+
+    protected static ?string $modelLabel = 'Lista';
+
+    protected static ?string $pluralModelLabel = 'Listas';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('bitrix')
+                            ->required()
+                            ->numeric(),
+                    ]),
+
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\Repeater::make('repeater')
+                            ->label('Item')
+                            ->schema([
+                                Forms\Components\TextInput::make('title')
+                                    ->label('Título')
+                                    ->required(),
+                            ])
+                            ->grid(4)
+                            ->required(),
+                    ])
+                    ->columns(1),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('bitrix')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Criado')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Atualizado')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListListings::route('/'),
+            'create' => Pages\CreateListing::route('/create'),
+            'edit' => Pages\EditListing::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+}
